5fdb5a5ef43bcb337ed71b9b2429f42ce2cc88e9
boram <boram2445@gmail.com>
2022-05-19 23:51:36 +0900

    [0519] 봉우리 문제 다시 풀이

--- inflearn-js/섹션2/24.js
+++ inflearn-js/섹션2/24.js
@@ -1,32 +1,28 @@
-/*각 격자에는 그 지역의 높이가 쓰여있습니다. 각 격자
+/* 24번 -  direct 
+각 격자에는 그 지역의 높이가 쓰여있습니다. 각 격자
 판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개 
 있는 지 알아내는 프로그램을 작성하세요.*/
 
 function solution(arr){  
-  
+  let answer = 0;
+  let len = arr.length; 
   //2중 팀색 -> z값으로 ny nx 구하기 
   let dy = [-1,0,1,0];
   let dx = [0,1,0,-1];
-  for(let y=0; y<arr.length; y++){
-    for(let x=0; x<arr.length; x++){
-      let flag = 0;
+  for(let y=0; y<len; y++){
+    for(let x=0; x<len; x++){
+      let flag = 1;
       for(let z=0; z<4; z++){
         let ny = dy[z]+y;
         let nx = dx[z]+x;
-        if(ny<0 ||nx<0 || ny>=length || nx>=length){
-          flag =1; 
+        if((ny>=0 && ny<n && nx>=0 && nx<n) && (arr[y][x]<=arr[ny][nx])){
+          flag=0; 
           break;
         }
-
       }
-      if(flag) break;
+      if(flag) answer++;
     }
   }
-
-
-  
-
-    
   return answer;
 }
 

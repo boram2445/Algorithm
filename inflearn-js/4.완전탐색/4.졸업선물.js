6d297805266f31de31d55f2c10724d2017dc03d5
boram <boram2445@gmail.com>
2023-08-01 23:29:13 +0900

    다른 풀이 추가

--- inflearn-js/4.완전탐색/4.졸업선물.js
+++ inflearn-js/4.완전탐색/4.졸업선물.js
@@ -18,7 +18,6 @@ function solution(m, product) {
 
   return answer;
 }
-
 let arr = [
   [6, 6],
   [2, 2],
@@ -27,3 +26,25 @@ let arr = [
   [10, 3],
 ];
 console.log(solution(28, arr));
+
+//선생님 풀이
+function solution2(m, product) {
+  let answer = 0;
+  let n = product.length;
+  product.sort((a, b) => (a[0] + a[1] > b[0] + b[1] ? 1 : -1)); //비싼 것대로 sort
+  for (let i = 0; i < product.length; i++) {
+    let money = m - (product[i][0] / 2 + product[i][1]); //할인받는 상품 선택
+    let cnt = 1;
+    for (let j = 0; j < n; j++) {
+      if (j !== i && money < product[j][0] + product[j][1]) break;
+      if (j !== i && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
+        cnt++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution2(28, arr));
